4b36c00245add64b51b16150afa0d00d769aae0f
JaiRaga <raga.j.santhosh@gmail.com>
2022-05-17 14:22:37 +0530

    :sparkles: stored and retreiving products from db

--- routes/products.js
+++ routes/products.js
@@ -1,20 +1,30 @@
 const express = require("express");
+const Product = require("../models/Product");
 const router = express.Router();
 
-const products = [
-  { id: 1, name: "comb", image: "img.url" },
-  { id: 2, name: "brush", image: "img.url" },
-];
-
-router.get("/", (req, res) => {
-  res.send(products);
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    if (!products) {
+      return res.status(404).json({ mesage: "no products found" });
+    }
+    res.status(200).json({ products, message: "success" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
-router.post("/", (req, res) => {
-  const newProduct = req.body;
-  console.log(newProduct);
-  products.unshift(newProduct)
-  res.send(products);
+router.post("/", async (req, res) => {
+  const product = new Product({ ...req.body });
+
+  try {
+    await product.save();
+    res.status(201).json({ product, message: "success" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
