35640b43b0bce1e1aa67be1a720a6acb4689b02b
JaiRaga <raga.j.santhosh@gmail.com>
2022-05-17 14:39:04 +0530

    :zap: enabled cors

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-require("dotenv/config");
-require("./db/mongoose")
-const express = require("express");
-const morgan = require('morgan')
+require('dotenv/config');
+require('./db/mongoose');
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors')
 
-const productsRouter = require("./routes/products")
-
+const productsRouter = require('./routes/products');
 const app = express();
-
 const api_version = process.env.API_VERSION;
 
-app.use(express.json())
-app.use(morgan('dev'))
-app.use(`${api_version}/products`, productsRouter)
+// enable cors
+app.use(cors())
+app.options('*', cors())
 
-
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(`${api_version}/products`, productsRouter);
 
 console.log(api_version);
 
