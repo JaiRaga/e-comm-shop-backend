4075bd5ddd83fa613fbf00e146af13edaee96b9f
JaiRaga <raga.j.santhosh@gmail.com>
2022-05-18 11:52:43 +0530

    :sparkles: created GET route to query a category by id and PUT route to update a category

--- routes/category.js
+++ routes/category.js
@@ -15,11 +15,44 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const category = await Category.findById(req.params.id);
+    if (!category) {
+      return res.status(404).json({ message: 'failure' });
+    }
+    res.status(200).json({ category, message: 'success' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'failure', error: err });
+  }
+});
+
 router.post('/', async (req, res) => {
   try {
     const category = new Category({ ...req.body });
     await category.save();
     res.status(201).json({ category, message: 'success' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'failure', error: err.errors });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const category = await Category.findByIdAndUpdate(
+      req.params.id,
+      {
+        ...req.body,
+      },
+      { new: true }
+    );
+    if (!category) {
+      return res.status(404).json({ message: 'failure' });
+    }
+
+    res.status(200).json({ category, message: 'success' });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'failure', error: err.errors });
