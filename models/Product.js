989adb166ba38095739f629e8b3ddfbed00a1d28
JaiRaga <raga.j.santhosh@gmail.com>
2022-05-18 10:54:12 +0530

    :saprkles: added fields for products

--- models/Product.js
+++ models/Product.js
@@ -1,25 +1,75 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const productSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 5,
-    maxlength: 100,
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 5,
+      maxlength: 100,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    richDescription: {
+      type: String,
+      default: '',
+    },
+    image: {
+      type: String,
+      required: true,
+      trim: true,
+      default: '',
+    },
+    images: [
+      {
+        type: String,
+      },
+    ],
+    brand: {
+      type: String,
+      default: '',
+    },
+    price: {
+      type: String,
+      default: 0,
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 500,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      default: 0,
+    },
+    isFeatured: {
+      type: Boolean,
+      default: false,
+    },
+    // dateCreated: {
+    //   type: Date,
+    //   default: Date.now,
+    // },
   },
-  image: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  countInStock: {
-    type: Number,
-    default: 1,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
-const Product = mongoose.model("Product", productSchema);
+const Product = mongoose.model('Product', productSchema);
 
 module.exports = Product;
