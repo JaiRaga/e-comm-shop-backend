9885e18512aa6e61e6cd4ef2b0b3c10432b50c27
JaiRaga <raga.j.santhosh@gmail.com>
2022-05-18 10:55:26 +0530

    :zap: added conditions to check if the products is null in GET route

--- routes/products.js
+++ routes/products.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const Product = require("../models/Product");
+const express = require('express');
+const Product = require('../models/Product');
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const products = await Product.find({});
     if (!products) {
-      return res.status(404).json({ mesage: "no products found" });
+      return res.status(404).json({ mesage: 'no products found' });
     }
-    res.status(200).json({ products, message: "success" });
+    res.status(200).json({ products, message: 'success' });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: "Internal Server Error" });
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 });
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   const product = new Product({ ...req.body });
 
   try {
     await product.save();
-    res.status(201).json({ product, message: "success" });
+    res.status(201).json({ product, message: 'success' });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: "Internal Server Error" });
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 });
 
