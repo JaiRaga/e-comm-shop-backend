620f6985cea9b4555f623c42e6a7c3d931cc4273
JaiRaga <raga.j.santhosh@gmail.com>
2022-05-19 15:05:32 +0530

    :sparkles: added method to select certain fields in a product list, and method to populate category based on condition

--- routes/products.js
+++ routes/products.js
@@ -1,21 +1,56 @@
 const express = require('express');
+const Category = require('../models/Category');
 const Product = require('../models/Product');
 const router = express.Router();
 
+// GET all products
 router.get('/', async (req, res) => {
+  // select fields based on query
+  let q = ''
+  for (let query in req.query) {
+    console.log('query..', query, req.query[query])
+    if (req.query[query] === 'true') {
+      q += query + ' '
+    }
+  }
+
+  // inorder to populate category
+  let populate = req.query['category'] === 'true' ? 'category' : ''
+
   try {
-    const products = await Product.find({});
+    const products = await Product.find({}).select(q).populate(populate);
     if (!products) {
       return res.status(404).json({ mesage: 'no products found' });
     }
     res.status(200).json({ products, message: 'success' });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Internal Server Error' });
+    res.status(500).json({ message: 'failure' });
   }
 });
 
+// GET product by id
+router.get('/:id', async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ mesage: 'no product found' });
+    }
+    res.status(200).json({ product, message: 'success' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'failure' });
+  }
+});
+
+// POST a product
 router.post('/', async (req, res) => {
+  // To check if the category in the body of the request is a valid category
+  const category = await Category.findById(req.body.category)
+  if (!category) {
+    return res.status(400).json({message: 'Invalid Category'})
+  }
+
   const product = new Product({ ...req.body });
 
   try {
@@ -23,7 +58,7 @@ router.post('/', async (req, res) => {
     res.status(201).json({ product, message: 'success' });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Internal Server Error' });
+    res.status(500).json({ message: 'failure', error: err.errors });
   }
 });
 
